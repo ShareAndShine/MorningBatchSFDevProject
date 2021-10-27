3f8fb2ba8bd536c34e5d6853a55b0df47960412b
readervm-Rajesh <reader.vm@gmail.com>
2021-10-27 12:36:20 +0530

    Added wire service hands on code

--- force-app/main/default/lwc/trainingTopicsList/trainingTopicsList.js
+++ force-app/main/default/lwc/trainingTopicsList/trainingTopicsList.js
@@ -1,4 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import fetchSFTrainingTopics from '@salesforce/apex/SFTopicHelper.getSFTrainingTopics';
+import fetchSFTrainingTopicsByCategory from '@salesforce/apex/SFTopicHelper.getSFTrainingTopicsByCategory';
+
+
 
 export default class TrainingTopicsList extends LightningElement {
 
@@ -11,11 +15,35 @@ export default class TrainingTopicsList extends LightningElement {
         { topicName : "JS Module",topicCategory : "Javascript"},
         { topicName : "Triggers",topicCategory : "APEX"},
     ];
+    recordExists = this.SFTopicsArr.length > 0 ? true: false; 
+
+    SFAllTrainingTopics;
+    
+    /*
+        1. Make a call to DB and retrieve all SF topics records
+            - create a SOQL query and use it inside APEX method
+            - Annotate the APEX method with @AuraEnabled(cachable=true)
+            - Use wire data service to make a call to the APEX method
+                -- Declare the method in JS file so that wire service can make a call 
+        2. Bind it in the html
+    */
+
+    //Method 1:Use @wire service and make a call to APEX Method and store output in a property
+    @wire(fetchSFTrainingTopics)
+    SFTopicslist; // Store OP in a prpoery and bind to HTML
+
+
+    //Method 2: Use @wire service and make a call to APEX Method and store output in a function
+    @wire(fetchSFTrainingTopics)
+    loadTrainingTopics({data,error})
+    {
+        this.SFAllTrainingTopics = data; //Assing APEX output to a local property then bind to HTML
+    }
+    
+
+    //Trying to access a method which is not annotated as auraenabled
+    @wire(fetchSFTrainingTopicsByCategory, {strCat: 'LWC'})
+    SFTopicsByCat;
 
     
-    recordExists = SFTopicsArr.length > 0 ? true: false; 
-
-
-    // Convert the above array of objects into a string so I can see readble date on the page
-    // SFTopicsArrConverted =  JSON.stringify(SFTopicsArr);
 }
